6dc1e6c6bdb736fdf8a4555f39d23965168509df
Alexsandro Gomes dias <126702081+Phospudim@users.noreply.github.com>
2023-06-01 16:24:59 -0300

    up
    update java script

---
+++ exercicios/ex09/ex0009.js
@@ -0,0 +1,10 @@
+var hora = 9
+console.log(`são ${hora} horas agora.`)
+
+if (hora >=8 && hora <=12) {
+    console.log(`está de manhã`)
+} else if (hora >12 && hora <17) {
+    console.log(`Está de tarde`)
+} else {
+    console.log(`É de noite.`)
+}
