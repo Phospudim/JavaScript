195d84d62635874f1149532c2346b800e67596a2
Alexsandro Gomes dias <126702081+Phospudim@users.noreply.github.com>
2023-07-06 11:38:35 -0300

    final java script

---
+++ exercicios/ex17/script.js
@@ -0,0 +1,4 @@
+var num = [5, 8 , 2 , 9 , 3]
+num[5] = 10
+num.length
+console.log(num.sort())
---
+++ exercicios/ex18/função.js
@@ -0,0 +1,8 @@
+ function parimpar(n) {
+    if (n%2 == 0 ) {
+        return 'par!'
+    } else {
+        return 'Ímpar!'
+    }
+ }
+ console.log(parimpar(223))
